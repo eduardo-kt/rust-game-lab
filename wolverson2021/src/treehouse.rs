cb51130398d028bb8ab14f2dac4227e55b14fef1
Eduardo K Teixeira <eduardo.kunzel@hotmail.com>
2025-07-11 14:30:31 -0300

    feat(treehouse): adiciona  what_is_your_name e Visitor
    - what_is_your_name(): retorna String da entrada do usuário
    - Visitor: struct com método new (construtor) e método greet_visitor (imprime greeting)

---
+++ wolverson2021/src/treehouse.rs
@@ -0,0 +1,30 @@
+//! biblioteca do exemplo c.2 p.20-
+use std::io::stdin;
+
+pub fn what_is_your_name() -> String {
+    println!("Hello, what's your name? ");
+    let mut your_name = String::new();
+    stdin()
+        .read_line(&mut your_name)
+        .expect("Failed to read line");
+
+    your_name.trim().to_lowercase()
+}
+
+pub struct Visitor {
+    pub name: String,
+    pub greeting: String,
+}
+
+impl Visitor {
+    pub fn new(name: &str, greeting: &str) -> Self {
+        Self {
+            name: name.to_lowercase(),
+            greeting: greeting.to_string() }
+    }
+
+    pub fn greet_visitor(&self) {
+        println!("{}", self.greeting);
+    }
+    
+}
