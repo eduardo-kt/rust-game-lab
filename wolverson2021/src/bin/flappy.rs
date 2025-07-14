0e8d0d1908810641afdfcb5973a5f326fe99e150
Eduardo K Teixeira <eduardo.kunzel@hotmail.com>
2025-07-14 09:46:42 -0300

    feat(flappy): adiciona struct State vazio

--- wolverson2021/src/bin/flappy.rs
+++ wolverson2021/src/bin/flappy.rs
@@ -1,6 +1,7 @@
 //! Código referente ao capítulo 3 do livro 
 //! Wolverson, H. (2021). Hands-On Rust (1st ed). Pragmatic Programmers, LLC.
 
-fn main() {
+struct State {}
+
     println!("Hello world flappy dragon");
 }
