dca071acbde4d93052f3521874300879613deedd
Eduardo K Teixeira <eduardo.kunzel@hotmail.com>
2025-07-14 07:47:52 -0300

    chore(treehouse): atualiza formatação do módulo com cargo fmt

--- wolverson2021/src/treehouse.rs
+++ wolverson2021/src/treehouse.rs
@@ -20,11 +20,11 @@ impl Visitor {
     pub fn new(name: &str, greeting: &str) -> Self {
         Self {
             name: name.to_lowercase(),
-            greeting: greeting.to_string() }
+            greeting: greeting.to_string(),
+        }
     }
 
     pub fn greet_visitor(&self) {
         println!("{}", self.greeting);
     }
-    
 }
