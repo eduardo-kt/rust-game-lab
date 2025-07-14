3d811f00b0044d1f232fe820e4a46f98fdbe6fc8
Eduardo K Teixeira <eduardo.kunzel@hotmail.com>
2025-07-14 11:07:11 -0300

    feat(flappy): adiciona struct Player

--- wolverson2021/src/bin/flappy.rs
+++ wolverson2021/src/bin/flappy.rs
@@ -1,5 +1,11 @@
 //! Código referente ao capítulo 3 do livro
 //! Wolverson, H. (2021). Hands-On Rust (1st ed). Pragmatic Programmers, LLC.
+
+struct Player {
+    x: i32,
+    y: i32,
+    velocity: f32,
+}
 enum GameMode {
     Menu,
     Playing,
