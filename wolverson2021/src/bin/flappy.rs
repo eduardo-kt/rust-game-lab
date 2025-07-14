5749751cc1febc677c06fc26e152ee7ea92ad1fb
Eduardo K Teixeira <eduardo.kunzel@hotmail.com>
2025-07-14 10:11:32 -0300

    feat(flappy): adiciona enum GameMode

--- wolverson2021/src/bin/flappy.rs
+++ wolverson2021/src/bin/flappy.rs
@@ -1,5 +1,10 @@
 //! Código referente ao capítulo 3 do livro
 //! Wolverson, H. (2021). Hands-On Rust (1st ed). Pragmatic Programmers, LLC.
+enum GameMode {
+    Menu,
+    Playing,
+    End,
+}
 
 struct State {}
 
