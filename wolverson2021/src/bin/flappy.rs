2cef58cc2ce4655f4e49f0f79c4fcfb4de13e782
Eduardo K Teixeira <eduardo.kunzel@hotmail.com>
2025-07-14 09:34:49 -0300

    feat(flappy): adiciona binário flappy

---
+++ wolverson2021/src/bin/flappy.rs
@@ -0,0 +1,6 @@
+//! Código referente ao capítulo 3 do livro 
+//! Wolverson, H. (2021). Hands-On Rust (1st ed). Pragmatic Programmers, LLC.
+
+fn main() {
+    println!("Hello world flappy dragon");
+}
